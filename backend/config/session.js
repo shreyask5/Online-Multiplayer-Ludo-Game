28c8597ab6465a22989faffe8051ce7855d900ba
shreyask5 <shreyasksh5@gmail.com>
2025-07-25 13:10:38 +0530

    Docker Update

--- backend/config/session.js
+++ backend/config/session.js
@@ -2,7 +2,7 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 
 const store = new MongoDBStore({
-    uri: process.env.CONNECTION_URI,
+    uri: process.env.MONGODB_URI,
     collection: 'sessions',
 });
 const sessionMiddleware = session({
